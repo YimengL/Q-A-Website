7e63894abdb5921d0ca68546cf499d71512a3830
Yimeng Li <lym1108csu@gmaill.com>
2018-06-16 23:06:45 -0400

    questionController init

---
+++ src/main/java/com/example/wenda/controller/QuestionController.java
@@ -0,0 +1,12 @@
+package com.example.wenda.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class QuestionController {
+    private static final Logger logger = LoggerFactory.getLogger(QuestionController.class);
+
+}
